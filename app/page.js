dae71b5cd753a3573eac936b17d8a7bcb2773e70
manjotsb <manjot.singh04@edu.sait.ca>
2024-09-23 23:26:11 -0600

    Week-2 Assignment

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      
+      <h1>CPRG 306: Web Development 2- Assignments</h1>
+      <p><Link href={"./week-2"}>Week-2 Assignments</Link></p>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function StudentInfo(){
+
+    return(
+    <main>
+        <h1>Manjot Singh</h1>
+        <Link href={"https://github.com/manjotsb"}>Github Link</Link>
+    </main>
+    );
+    
+}
